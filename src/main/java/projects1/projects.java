dd3ece6cad2920a72a25bb5f13f668fbf7a785f2
Michael O'Neil <michaeloneil2993@gmail.com>
2023-08-23 22:34:47 -0400

    my first commit

---
+++ src/main/java/projects1/projects.java
@@ -0,0 +1,15 @@
+package projects1;
+
+import projects.dao.DbConnection;
+import projects.exception.DbException;
+
+public class projects {
+
+	public static void main(String[] args) {
+		
+		DbConnection.getConnection(); 
+		
+
+	}
+
+}
